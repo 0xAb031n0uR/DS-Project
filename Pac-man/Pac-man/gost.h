8e8b4f0836bb77ef4e0d819943e430890b7a58cc
mostafa aboelnour <mostafaaboelnour19@gmail.com>
2021-05-24 01:35:54 +0200

    ghost updated but not completed

--- Pac-man/Pac-man/gost.h
+++ Pac-man/Pac-man/gost.h
@@ -1,5 +1,34 @@
-#pragma once
+#include<iostream>
+#include<vector>
+#include<SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
 class gost
 {
-};
+public:
+    sf::Texture gt;
+    sf::RectangleShape gs;
 
+    bool vis[19][20] = { 0 };
+    struct path
+    {
+        int x;
+        int y;
+        int brv;
+    };
+    struct npair
+    {
+        int x;
+        int y;
+    };
+    vector<path>nodes;
+    vector<npair>final_path;
+    int dx[4] = { 1,-1,0,0 };
+    int dy[4] = { 0,0,-1,1 };
+    gost(sf::RenderWindow& window, string s, int x, int y);
+    void move();
+    void display();
+    void BFS(int x_position, int y_position, int t_x, int t_y, int maze_arr[23][23]);
+
+    ///////////////////////////////////////
+};
